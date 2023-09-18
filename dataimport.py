7d144056ef4246563d32580f92bbf1fe0f4462f9
nervewastaken <krishverma2004@gmail.com>
2023-09-19 02:08:49 +0530

    Update dataimport.py

--- dataimport.py
+++ dataimport.py
@@ -1,4 +1,10 @@
 import pandas as pd
 ap = pd.read_csv('apple.csv')
 
+
+##print(ap.head())
+ap['Price1']=ap['Close'].shift(-1)
+ap['PriceDiff'] = ap['Price1']-ap['Close']
+ap['Direction'] = [1 if ap.loc[ei,'PriceDiff']>0 else -1
+                   for ei in ap.index]
 print(ap.head())
