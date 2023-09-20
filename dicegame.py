e2e2f3a0f2f8b28adb6378e63e1fd702bfbe65cb
nervewastaken <krishverma2004@gmail.com>
2023-09-20 13:15:11 +0530

    Update dicegame.py

--- dicegame.py
+++ dicegame.py
@@ -1,5 +1,19 @@
 import pandas as pd
+import numpy as np
+import matplotlib as plt
 
-die = pd.DataFrame([1,2,3,4,5,6])
-sum_of_dice = die.sample(2,replace=True).sum().loc[0]
-print('Sum of dice is',sum_of_dice)
+die = pd.DataFrame(np.arange(1, 7))  # Create a DataFrame with values 1 to 6
+
+sum_of_dice = die.sample(2, replace=True).sum().iloc[0]  # Sample 2 values and calculate their sum
+
+results = []  # Create an empty list to store the results
+
+for x in range(50):
+    result = die.sample(2, replace=True).sum().iloc[0]  # Simulate rolling 2 dice and calculate the sum
+    results.append(result)
+
+freq = pd.DataFrame(results)[0].value_counts()
+sort_freq = freq.sort_index()
+
+sort_freq.plot()
+
